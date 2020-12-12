7112381111458ac979d5dc72dab35b9f711bd2cc
Victor Porton <porton@narod.ru>
2020-12-13 00:39:20 +0200

    Solidity version updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,6 +18,6 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.7.5",
 };
 
