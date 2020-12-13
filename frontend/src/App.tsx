d73f0c77aefc101868b641b5c750d5ff0af5c400
Victor Porton <porton@narod.ru>
2020-12-14 00:29:16 +0200

    restructured

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,16 +6,40 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Manage Smart Crypto Funds</h1>
-        <p>ERC-20 token address: <input type="text" maxLength={42} size={50}/></p>
-        <p>ERC-1155 locker contract address: <input type="text" maxLength={78} size={92}/></p>
+        <p>ERC-20 token address: <Address/></p>
+        <p>ERC-1155 locker contract address: <Uint256/></p>
         <p>
-          Amount: <input type="number"/>
+          Amount: <Amount/>
           <input type="button" value="Swap ERC-1155 to ERC-20"/>
           <input type="button" value="Swap ERC-20 to ERC-1155"/>
         </p>
       </header>
     </div>
   );
+}
+
+function Address() {
+  return (
+    <span className="Address">
+      <input type="text" maxLength={42} size={50}/>
+    </span>
+  )
+}
+
+function Uint256() {
+  return (
+    <span className="Uint256">
+      <input type="text" maxLength={78} size={92}/>
+    </span>
+  )
+}
+
+function Amount() {
+  return (
+    <span className="Amount">
+      <input type="number"/>
+    </span>
+  )
 }
 
 export default App;
