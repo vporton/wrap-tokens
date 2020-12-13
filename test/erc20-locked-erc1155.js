0447ad684ce91a2f63f1034132cf6107438586f3
Victor Porton <porton@narod.ru>
2020-12-13 20:17:55 +0200

    better testing

--- test/erc20-locked-erc1155.js
+++ test/erc20-locked-erc1155.js
@@ -57,17 +57,35 @@ describe("ERC20LockedERC1155", function() {
     this.wrapper.connect(this.user1).returnToERC1155(parseEther("600"), this.user1.address);
     expect(await this.erc1155Mock.balanceOf(this.user1.address, tokenId)).to.equal(parseEther("600"));
     expect(await this.wrapper.balanceOf(this.user1.address)).to.equal(parseEther("0"));
+  });
 
+  it("increase/decrease allowance", async function() {
+    this.wrapper.connect(this.user1).borrowERC1155(parseEther("1000"), this.user1.address, this.user1.address);
+
+    await this.wrapper.connect(this.user1).approve(this.user3.address, parseEther("300"));
     const self = this;
     {
       async function fails() {
-        await self.wrapper.connect(self.user2).transferFrom(self.user3.address, self.user2.address, parseEther("10"));
+        await self.wrapper.connect(self.user3).transferFrom(self.user1.address, self.user2.address, parseEther("400"));
+      }
+      await expectThrowsAsync(fails, "VM Exception while processing transaction: revert ERC20: transfer amount exceeds allowance");
+    }
+    await this.wrapper.connect(this.user1).approve(this.user3.address, parseEther("600"));
+    await this.wrapper.connect(this.user3).transferFrom(this.user1.address, this.user2.address, parseEther("500"));
+    expect(await this.wrapper.allowance(this.user1.address, this.user3.address)).to.equal(parseEther("100"));
+    await this.wrapper.connect(this.user1).increaseAllowance(this.user3.address, parseEther("200"));
+    expect(await this.wrapper.allowance(this.user1.address, this.user3.address)).to.equal(parseEther("300"));
+    await this.wrapper.connect(this.user1).decreaseAllowance(this.user3.address, parseEther("100"));
+    expect(await this.wrapper.allowance(this.user1.address, this.user3.address)).to.equal(parseEther("200"));
+    {
+      async function fails() {
+        await self.wrapper.connect(self.user1).decreaseAllowance(self.user3.address, parseEther("10000"));
       }
       await expectThrowsAsync(fails, "VM Exception while processing transaction: revert ERC20: transfer amount exceeds allowance");
     }
   });
 
-  it("metadata", async function() {
+it("metadata", async function() {
     expect(await this.wrapper.name()).to.equal("Test coin");
     expect(await this.wrapper.symbol()).to.equal("MCK");
     expect(await this.wrapper.decimals()).to.equal(18);
--- test/erc20-over-erc1155.js
+++ test/erc20-over-erc1155.js
@@ -48,13 +48,29 @@ describe("ERC20OverERC1155", function() {
     expect(await this.erc1155Mock.balanceOf(this.user2.address, tokenId)).to.equal(parseEther("400"));
     expect(await this.wrapper.balanceOf(this.user2.address)).to.equal(parseEther("400"));
 
-    const self = this;
-    {
-      async function fails() {
-        await self.wrapper.connect(self.user2).transferFrom(self.user3.address, self.user2.address, parseEther("10"));
+    it("increase/decrease allowance", async function() {
+      await this.wrapper.connect(this.user1).approve(this.user3.address, parseEther("300"));
+      const self = this;
+      {
+        async function fails() {
+          await self.wrapper.connect(self.user3).transferFrom(self.user1.address, self.user2.address, parseEther("400"));
+        }
+        await expectThrowsAsync(fails, "VM Exception while processing transaction: revert ERC20: transfer amount exceeds allowance");
       }
-      await expectThrowsAsync(fails, "VM Exception while processing transaction: revert ERC20: transfer amount exceeds allowance");
-    }
+      await this.wrapper.connect(this.user1).approve(this.user3.address, parseEther("600"));
+      await this.wrapper.connect(this.user3).transferFrom(this.user1.address, this.user2.address, parseEther("500"));
+      expect(await this.wrapper.allowance(this.user1.address, this.user3.address)).to.equal(parseEther("100"));
+      await this.wrapper.connect(this.user1).increaseAllowance(this.user3.address, parseEther("200"));
+      expect(await this.wrapper.allowance(this.user1.address, this.user3.address)).to.equal(parseEther("300"));
+      await this.wrapper.connect(this.user1).decreaseAllowance(this.user3.address, parseEther("100"));
+      expect(await this.wrapper.allowance(this.user1.address, this.user3.address)).to.equal(parseEther("200"));
+      {
+        async function fails() {
+          await self.wrapper.connect(self.user1).decreaseAllowance(self.user3.address, parseEther("10000"));
+        }
+        await expectThrowsAsync(fails, "VM Exception while processing transaction: revert ERC20: transfer amount exceeds allowance");
+      }
+    });
 
     it("metadata", async function() {
       expect(await this.wrapper.name()).to.equal("Test coin");
