9c12c1605a113c5959f14f3b3876ade765e32849
Victor Porton <porton@narod.ru>
2020-12-13 16:44:15 +0200

    tests improved

--- test/erc1155-locked-erc20.js
+++ test/erc1155-locked-erc20.js
@@ -23,40 +23,44 @@ describe("ERC1155LockedERC20", function() {
   it("ERC20 locked in ERC1155", async function() {
     const [deployer, user1, user2, user3] = await ethers.getSigners();
 
-    const ERC20Mock = await ethers.getContractFactory("MockCoin");
-    const erc20Mock = await ERC20Mock.deploy();
-    await erc20Mock.deployed();
-    erc20Mock.mint(user3.address, parseEther("1000"), []);
-    
-    const erc1155LockedERC20 = await ethers.getContractFactory("ERC1155LockedERC20");
-    const wrapper = await erc1155LockedERC20.deploy("https://example.com");
-    await wrapper.deployed();
+    beforeEach(async function() {
+      const ERC20Mock = await ethers.getContractFactory("MockCoin");
+      this.erc20Mock = await ERC20Mock.deploy();
+      await this.erc20Mock.deployed();
+      this.erc20Mock.mint(user3.address, parseEther("1000"), []);
+      
+      const erc1155LockedERC20 = await ethers.getContractFactory("ERC1155LockedERC20");
+      this.wrapper = await erc1155LockedERC20.deploy("https://example.com");
+      await this.wrapper.deployed();
 
-    await erc20Mock.connect(user3).approve(wrapper.address, BigNumber.from(2).pow(BigNumber.from(256)).sub(BigNumber.from(1)));
-    wrapper.connect(user3).borrowERC20(erc20Mock.address, parseEther("1000"), user3.address, user1.address, []);
-    expect(await erc20Mock.balanceOf(user1.address)).to.equal(parseEther("0"));
-    expect(await wrapper.balanceOf(user1.address, erc20Mock.address)).to.equal(parseEther("1000"));
+      await this.erc20Mock.connect(user3).approve(this.wrapper.address, BigNumber.from(2).pow(BigNumber.from(256)).sub(BigNumber.from(1)));
+      this.wrapper.connect(user3).borrowERC20(this.erc20Mock.address, parseEther("1000"), user3.address, user1.address, []);
+      expect(await this.erc20Mock.balanceOf(user1.address)).to.equal(parseEther("0"));
+      expect(await this.wrapper.balanceOf(user1.address, this.erc20Mock.address)).to.equal(parseEther("1000"));
+    });
 
-    await wrapper.connect(user1).safeTransferFrom(user1.address, user2.address, erc20Mock.address, parseEther("300"), []);
-    expect(await wrapper.balanceOf(user1.address, erc20Mock.address)).to.equal(parseEther("700"));
-    expect(await wrapper.balanceOf(user2.address, erc20Mock.address)).to.equal(parseEther("300"));
+    it("ERC20 locked in ERC155", async function() {
+      await this.wrapper.connect(user1).safeTransferFrom(user1.address, user2.address, this.erc20Mock.address, parseEther("300"), []);
+      expect(await this.wrapper.balanceOf(user1.address, this.erc20Mock.address)).to.equal(parseEther("700"));
+      expect(await this.wrapper.balanceOf(user2.address, this.erc20Mock.address)).to.equal(parseEther("300"));
 
-    {
-      async function fails() {
-        await wrapper.connect(user3).safeTransferFrom(user1.address, user2.address, erc20Mock.address, parseEther("100"), []);
+      {
+        async function fails() {
+          await this.wrapper.connect(user3).safeTransferFrom(user1.address, user2.address, this.erc20Mock.address, parseEther("100"), []);
+        }
+        // TODO: Here it's a different error message that in ERC1155OverERC20.
+        await expectThrowsAsync(fails, "VM Exception while processing transaction: revert ERC1155: caller is not owner nor approved");
       }
-      // TODO: Here it's a different error message that in ERC1155OverERC20.
-      await expectThrowsAsync(fails, "VM Exception while processing transaction: revert ERC1155: caller is not owner nor approved");
-    }
-    await wrapper.connect(user1).setApprovalForAll(user3.address, true);
-    await wrapper.connect(user3).safeTransferFrom(user1.address, user2.address, erc20Mock.address, parseEther("100"), []);
-    expect(await erc20Mock.balanceOf(user1.address)).to.equal(parseEther("0"));
-    expect(await wrapper.balanceOf(user1.address, erc20Mock.address)).to.equal(parseEther("600"));
-    expect(await erc20Mock.balanceOf(user2.address)).to.equal(parseEther("0"));
-    expect(await wrapper.balanceOf(user2.address, erc20Mock.address)).to.equal(parseEther("400"));
+      await this.wrapper.connect(user1).setApprovalForAll(user3.address, true);
+      await this.wrapper.connect(user3).safeTransferFrom(user1.address, user2.address, this.erc20Mock.address, parseEther("100"), []);
+      expect(await this.erc20Mock.balanceOf(user1.address)).to.equal(parseEther("0"));
+      expect(await this.wrapper.balanceOf(user1.address, this.erc20Mock.address)).to.equal(parseEther("600"));
+      expect(await this.erc20Mock.balanceOf(user2.address)).to.equal(parseEther("0"));
+      expect(await this.wrapper.balanceOf(user2.address, this.erc20Mock.address)).to.equal(parseEther("400"));
 
-    wrapper.connect(user1).returnToERC20(erc20Mock.address, parseEther("600"), user3.address);
-    expect(await erc20Mock.balanceOf(user3.address)).to.equal(parseEther("600"));
+      this.wrapper.connect(user1).returnToERC20(this.erc20Mock.address, parseEther("600"), user3.address);
+      expect(await this.erc20Mock.balanceOf(user3.address)).to.equal(parseEther("600"));
+    });
 
     // TODO: Check batch transfers.
   });
--- test/erc1155-over-erc20.js
+++ test/erc1155-over-erc20.js
@@ -23,42 +23,46 @@ describe("ERC1155OverERC20", function() {
   it("Transferring ERC20 through ERC1155", async function() {
     const [deployer, user1, user2, user3] = await ethers.getSigners();
 
-    const ERC20Mock = await ethers.getContractFactory("MockCoin");
-    const erc20Mock = await ERC20Mock.deploy();
-    await erc20Mock.deployed();
-    erc20Mock.mint(user1.address, parseEther("1000"));
-    
-    const ERC1155OverERC20 = await ethers.getContractFactory("ERC1155OverERC20");
-    const wrapper = await ERC1155OverERC20.deploy("https://example.com");
-    await wrapper.deployed();
+    beforeEach(async function() {
+      const ERC20Mock = await ethers.getContractFactory("MockCoin");
+      this.erc20Mock = await ERC20Mock.deploy();
+      await this.erc20Mock.deployed();
+      this.erc20Mock.mint(user1.address, parseEther("1000"));
+      
+      const ERC1155OverERC20 = await ethers.getContractFactory("ERC1155OverERC20");
+      this.wrapper = await ERC1155OverERC20.deploy("https://example.com");
+      await this.wrapper.deployed();
 
-    await erc20Mock.connect(user1).approve(wrapper.address, BigNumber.from(2).pow(BigNumber.from(256)).sub(BigNumber.from(1)));
+      await this.erc20Mock.connect(user1).approve(this.wrapper.address, BigNumber.from(2).pow(BigNumber.from(256)).sub(BigNumber.from(1)));
+    });
 
-    await wrapper.connect(user1).safeTransferFrom(
-      user1.address,
-      user2.address,
-      erc20Mock.address,
-      parseEther("300"),
-      []
-    );
+    it("ERC1155 over ERC20", async function() {
+      await this.wrapper.connect(user1).safeTransferFrom(
+        user1.address,
+        user2.address,
+        this.erc20Mock.address,
+        parseEther("300"),
+        []
+      );
 
-    expect(await erc20Mock.balanceOf(user1.address)).to.equal(parseEther("700"));
-    expect(await wrapper.balanceOf(user1.address, erc20Mock.address)).to.equal(parseEther("700"));
-    expect(await erc20Mock.balanceOf(user2.address)).to.equal(parseEther("300"));
-    expect(await wrapper.balanceOf(user2.address, erc20Mock.address)).to.equal(parseEther("300"));
+      expect(await this.erc20Mock.balanceOf(user1.address)).to.equal(parseEther("700"));
+      expect(await this.wrapper.balanceOf(user1.address, this.erc20Mock.address)).to.equal(parseEther("700"));
+      expect(await this.erc20Mock.balanceOf(user2.address)).to.equal(parseEther("300"));
+      expect(await this.wrapper.balanceOf(user2.address, this.erc20Mock.address)).to.equal(parseEther("300"));
 
-    {
-      async function fails() {
-        await wrapper.connect(user3).safeTransferFrom(user1.address, user2.address, erc20Mock.address, parseEther("100"), []);
+      {
+        async function fails() {
+          await this.wrapper.connect(user3).safeTransferFrom(user1.address, user2.address, this.erc20Mock.address, parseEther("100"), []);
+        }
+        await expectThrowsAsync(fails, "VM Exception while processing transaction: revert ERC1155: Transfer not approved");
       }
-      await expectThrowsAsync(fails, "VM Exception while processing transaction: revert ERC1155: Transfer not approved");
-    }
-    await wrapper.connect(user1).setApprovalForAll(user3.address, true);
-    await wrapper.connect(user3).safeTransferFrom(user1.address, user2.address, erc20Mock.address, parseEther("100"), []);
-    expect(await erc20Mock.balanceOf(user1.address)).to.equal(parseEther("600"));
-    expect(await wrapper.balanceOf(user1.address, erc20Mock.address)).to.equal(parseEther("600"));
-    expect(await erc20Mock.balanceOf(user2.address)).to.equal(parseEther("400"));
-    expect(await wrapper.balanceOf(user2.address, erc20Mock.address)).to.equal(parseEther("400"));
+      await this.wrapper.connect(user1).setApprovalForAll(user3.address, true);
+      await this.wrapper.connect(user3).safeTransferFrom(user1.address, user2.address, this.erc20Mock.address, parseEther("100"), []);
+      expect(await this.erc20Mock.balanceOf(user1.address)).to.equal(parseEther("600"));
+      expect(await this.wrapper.balanceOf(user1.address, this.erc20Mock.address)).to.equal(parseEther("600"));
+      expect(await this.erc20Mock.balanceOf(user2.address)).to.equal(parseEther("400"));
+      expect(await this.wrapper.balanceOf(user2.address, this.erc20Mock.address)).to.equal(parseEther("400"));
+    });
 
     // TODO: Check batch transfers.
   });
