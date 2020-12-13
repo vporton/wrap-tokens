a532447501d993bb5c8d38538ee0f5e6003bb897
Victor Porton <porton@narod.ru>
2020-12-13 18:38:03 +0200

    tests improved

--- test/erc20-over-erc1155.js
+++ test/erc20-over-erc1155.js
@@ -2,11 +2,26 @@ const { expect } = require("chai");
 const { BigNumber } = ethers;
 const { parseEther } = ethers.utils;
 
+// TODO: Duplicate code.
+const expectThrowsAsync = async (method, errorMessage) => {
+  let error = null
+  try {
+    await method()
+  }
+  catch (err) {
+    error = err
+  }
+  expect(error).to.be.an('Error')
+  if (errorMessage) {
+    expect(error.message).to.equal(errorMessage)
+  }
+}
+
 describe("ERC20OverERC1155", function() {
   const tokenId = 124;
 
   beforeEach(async function() {
-    [this.deployer, this.user1, this.user2] = await ethers.getSigners();
+    [this.deployer, this.user1, this.user2, this.user3] = await ethers.getSigners();
 
     const ERC1155Mock = await ethers.getContractFactory("ERC1155Mock");
     this.erc1155Mock = await ERC1155Mock.deploy("https://example.com");
@@ -32,6 +47,15 @@ describe("ERC20OverERC1155", function() {
     expect(await this.wrapper.balanceOf(this.user1.address)).to.equal(parseEther("600"));
     expect(await this.erc1155Mock.balanceOf(this.user2.address, tokenId)).to.equal(parseEther("400"));
     expect(await this.wrapper.balanceOf(this.user2.address)).to.equal(parseEther("400"));
+
+    const self = this;
+    {
+      async function fails() {
+        await self.wrapper.connect(self.user2).transferFrom(self.user3.address, self.user2.address, parseEther("10"));
+      }
+      // TODO: The error message is different here.
+      await expectThrowsAsync(fails, "VM Exception while processing transaction: revert ERC20: decreased allowance below zero");
+    }
   });
 
   // TODO: Check that can't transfer for other users without their approval.
