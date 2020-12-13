aabe7a9d29ea78f13625e2635cb15053b49a32ba
Victor Porton <porton@narod.ru>
2020-12-13 23:25:46 +0200

    frontend started

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,10 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("hardhat-deploy-ethers");
 require("hardhat-deploy");
+require("@symfoni/hardhat-react");
+require("hardhat-typechain");
+require("typechain-target-ethers-v5");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
