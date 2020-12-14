155275b4fcb59fd4b2f5ac9c4791c4582bff5944
Victor Porton <porton@narod.ru>
2020-12-14 07:08:50 +0200

    removed hardhat-frontend remnants

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,9 +2,9 @@ require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require("hardhat-deploy-ethers");
 require("hardhat-deploy");
-require("@symfoni/hardhat-react");
-require("hardhat-typechain");
-require("typechain-target-ethers-v5");
+// require("@symfoni/hardhat-react");
+// require("hardhat-typechain");
+// require("typechain-target-ethers-v5");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
