b2aef5cfafd9c5ef7c6a96d61e18e49207a2a237
Victor Porton <porton@narod.ru>
2020-12-14 08:25:44 +0200

    frontend/public/addresses.json

--- deploy/ERC20LockedERC1155.js
+++
@@ -1,6 +0,0 @@
-const myDeploy = require('../lib/wrapper-deployer');
-const contractName = 'ERC20LockedERC1155';
-const uuid = 'ef754770-3dd1-11eb-bcaf-cf9f6068b160';
-
-module.exports = myDeploy(contractName, `urn:uuid:${uuid}`);
-module.exports.tags = [contractName];
--- deploy/ERC20OverERC1155.js
+++
@@ -1,6 +0,0 @@
-const myDeploy = require('../lib/wrapper-deployer');
-const contractName = 'ERC20OverERC1155';
-const uuid = '000b0bd8-3dd2-11eb-a46d-abb98f9bd133';
-
-module.exports = myDeploy(contractName, `urn:uuid:${uuid}`);
-module.exports.tags = [contractName];
