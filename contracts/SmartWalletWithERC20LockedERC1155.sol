a99230b51bc21a87da3566b93f8bfabb245a6ae4
Victor Porton <porton@narod.ru>
2020-12-15 02:41:51 +0200

    incorporated main

--- contracts/SmartWalletWithERC20LockedERC1155.sol
+++ contracts/SmartWalletWithERC20LockedERC1155.sol
@@ -5,6 +5,6 @@ import "argent-contracts/contracts/wallet/BaseWallet.sol";
 import "./ERC20LockedERC1155.sol";
 // import "./ERC1155Holder.sol";
 
-contract SmartWalletWithERC1155LockedERC20 is BaseWallet, ERC20LockedERC1155/*, ERC1155Holder*/ {
+contract SmartWalletWithERC20LockedERC1155 is BaseWallet, ERC20LockedERC1155/*, ERC1155Holder*/ {
     constructor(IMyERC1155 _erc1155, uint256 _tokenId) ERC20LockedERC1155(_erc1155, _tokenId) { }
 }
---
+++ scripts/export-bytecode.js
@@ -0,0 +1,43 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional 
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+const fs = require('fs');
+
+function exportContract(name) {
+    const text = fs.readFileSync(`tmp/${name}.json`);
+    const json = JSON.parse(text);
+    fs.writeFileSync(`frontend/public/bytecode/${name}.bytecode`, json.bytecode);
+}
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile 
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  const contracts = [
+    'ERC20OverERC1155',
+    'ERC20LockedERC1155',
+    'SmartWalletWithERC1155LockedERC20',
+    'SmartWalletWithERC1155OverERC20',
+    'SmartWalletWithERC20LockedERC1155',
+    'SmartWalletWithERC20OverERC1155'
+  ];
+  for (let name of contracts) {
+    exportContract(name);
+  }
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
