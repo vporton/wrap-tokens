f3730ef2f8a866f2a83a6fc5a2b120545739bb33
Victor Porton <porton@narod.ru>
2020-12-17 03:06:17 +0200

    set app title

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -11,6 +11,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+document.title = "Convert ERC-1155 <-> ERC-20";
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
