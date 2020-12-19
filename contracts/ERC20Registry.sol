29e35dc36c31fd456d75f9b212bfb1b320f5619e
Victor Porton <porton@narod.ru>
2020-12-19 10:33:08 +0200

    FIXME comments

--- contracts/ERC20Registry.sol
+++ contracts/ERC20Registry.sol
@@ -1,6 +1,9 @@
 //SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.7.1;
 
+// FIXME: Use proxy.
+// FIXME: Deploy multiple contracts by one call.
+
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import './ERC20OverERC1155.sol';
 import './ERC20LockedERC1155.sol';
