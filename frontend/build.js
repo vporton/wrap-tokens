9c2e50e8dce4a36a42fcb1885966b486700af72b
Victor Porton <porton@narod.ru>
2020-12-20 23:23:05 +0200

    added Google Analytics

---
+++ frontend/build.js
@@ -0,0 +1,23 @@
+const execSync = require('child_process').execSync;
+const fs = require('fs');
+
+execSync("npx react-scripts build");
+
+fs.writeFileSync("build/CNAME", "erc1155.portonvictor.org");
+
+const inhead = `
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6ZX3GEN0J8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6ZX3GEN0J8');
+</script>
+`;
+
+const indexFileName = "build/index.html";
+let index = fs.readFileSync(indexFileName, {encoding: 'utf-8'});
+index = index.replace("<head>", "<head>" + inhead);
+fs.writeFileSync(indexFileName, index);
