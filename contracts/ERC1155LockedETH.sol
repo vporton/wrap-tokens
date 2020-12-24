4ebc8c5770a11b153191c17227569c9d27d0094f
Victor Porton <porton@narod.ru>
2020-12-24 12:25:20 +0200

    ERC1155LockedETH

---
+++ contracts/ERC1155LockedETH.sol
@@ -0,0 +1,51 @@
+//SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.7.1;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./ERC1155.sol";
+import "./IERC1155Views.sol";
+
+contract ERC1155LockedETH is ERC1155 {
+    uint256 constant tokenId = 0;
+
+    // solhint-disable func-visibility
+    constructor (string memory uri_) ERC1155(uri_) {
+    }
+    // solhint-enable func-visibility
+
+    function borrowETH(address _to, bytes calldata _data) public payable {
+        _mint(_to, tokenId, msg.value, _data);
+        emit BorrowedETH(msg.sender, msg.value, _to, _data);
+    }
+
+    function returnToETH(uint256 _amount, address payable _to) public {
+        _to.transfer(_amount);
+        _burn(msg.sender, tokenId, _amount);
+        emit ReturnedToETH(_amount, msg.sender, _to);
+    }
+
+    function name(uint256 _id) external pure ourTokenId(_id) returns (string memory) {
+        return "Wrapped Ether";
+    }
+
+    function symbol(uint256 _id) external pure ourTokenId(_id) returns (string memory) {
+        return "wETH";
+    }
+
+    function decimals(uint256 _id) external pure ourTokenId(_id) returns (uint8) {
+        return 18;
+    }
+
+    function uri(uint256 _id) external view override ourTokenId(_id) returns (string memory) {
+        return _uri;
+    }
+
+    modifier ourTokenId(uint256 _id) {
+        require(_id == tokenId, "undefined token ID");
+        _;
+    }
+
+    event BorrowedETH(address sender, uint256 amount, address to, bytes data);
+
+    event ReturnedToETH(uint256 amount, address from, address to);
+}
---
+++ test/erc1155-locked-eth.js
@@ -0,0 +1,107 @@
+const chai = require("chai");
+const { expect } = chai;
+const { BigNumber } = ethers;
+const { toBN } = BigNumber;
+const { provider } = ethers;
+const { parseEther } = ethers.utils;
+
+chai.use(require('chai-as-promised'))
+
+// TODO: Duplicate code.
+const expectThrowsAsync = async (method, errorMessage) => {
+  let error = null
+  try {
+    await method()
+  }
+  catch (err) {
+    error = err
+  }
+  expect(error).to.be.an('Error')
+  if (errorMessage) {
+    expect(error.message).to.equal(errorMessage)
+  }
+}
+
+const tokenId = 0;
+
+describe("ERC1155LockedETH", function() {
+  beforeEach(async function() {
+    [this.deployer, this.user1, this.user2, this.user3] = await ethers.getSigners();
+
+    const erc1155LockedERC20 = await ethers.getContractFactory("ERC1155LockedETH");
+    this.wrapper = await erc1155LockedERC20.deploy("https://example.com");
+    await this.wrapper.deployed();
+
+    this.wrapper.connect(this.user3).borrowETH(this.user1.address, [], {value: parseEther("10")});
+    // expect(await provider.getBalance(this.user1.address)).to.equal(parseEther("0"));
+    expect(await this.wrapper.balanceOf(this.user1.address, tokenId)).to.equal(parseEther("10"));
+  });
+
+  it("ETH locked in ERC1155", async function() {
+    await this.wrapper.connect(this.user1).safeTransferFrom(this.user1.address, this.user2.address, tokenId, parseEther("3"), []);
+    expect(await this.wrapper.balanceOf(this.user1.address, tokenId)).to.equal(parseEther("7"));
+    expect(await this.wrapper.balanceOf(this.user2.address, tokenId)).to.equal(parseEther("3"));
+
+    const self = this;
+    {
+      async function fails() {
+        await self.wrapper.connect(self.user3).safeTransferFrom(self.user1.address, self.user2.address, tokenId, parseEther("1"), []);
+      }
+      await expectThrowsAsync(fails, "VM Exception while processing transaction: revert ERC1155: caller is not owner nor approved");
+    }
+    await this.wrapper.connect(this.user1).setApprovalForAll(this.user3.address, true);
+    await this.wrapper.connect(this.user3).safeTransferFrom(this.user1.address, this.user2.address, tokenId, parseEther("1"), []);
+    // expect(await provider.getBalance(this.user1.address)).to.equal(parseEther("0"));
+    expect(await this.wrapper.balanceOf(this.user1.address, tokenId)).to.equal(parseEther("6"));
+    // expect(await provider.getBalance(this.user2.address)).to.equal(parseEther("0"));
+    expect(await this.wrapper.balanceOf(this.user2.address, tokenId)).to.equal(parseEther("4"));
+
+    this.wrapper.connect(this.user1).returnToETH(parseEther("6"), this.user3.address);
+    // expect(await provider.getBalance(this.user3.address)).to.equal(parseEther("6"));
+  });
+
+  it("ETH locked in ERC155 (batch transfers)", async function() {
+    await this.wrapper.connect(this.user1).safeBatchTransferFrom(this.user1.address, this.user2.address, [tokenId], [parseEther("3")], []);
+    // TODO: Heterogeneous arrays of users.
+    expect(await this.wrapper.balanceOfBatch([this.user1.address, this.user1.address], [tokenId, tokenId])).to.deep.equal([parseEther("7"), parseEther("7")]);
+    expect(await this.wrapper.balanceOfBatch([this.user2.address, this.user2.address], [tokenId, tokenId])).to.deep.equal([parseEther("3"), parseEther("3")]);
+
+    const self = this;
+    {
+      async function fails() {
+        await self.wrapper.connect(self.user3).safeBatchTransferFrom(self.user1.address, self.user3.address, [tokenId, tokenId], [parseEther("1"), parseEther("1")], []);
+      }
+      await expectThrowsAsync(fails, "VM Exception while processing transaction: revert ERC1155: caller is not owner nor approved");
+    }
+    expect(await this.wrapper.connect(this.user1).isApprovedForAll(this.user1.address, this.user3.address)).to.be.equal(false);
+    await this.wrapper.connect(this.user1).setApprovalForAll(this.user3.address, true);
+    expect(await this.wrapper.connect(this.user1).isApprovedForAll(this.user1.address, this.user3.address)).to.be.equal(true);
+    await this.wrapper.connect(this.user3).safeBatchTransferFrom(this.user1.address, this.user3.address, [tokenId], [parseEther("1")], []);
+    // expect(await provider.getBalance(this.user1.address)).to.equal(parseEther("0"));
+    // TODO: Heterogeneous arrays of users.
+    expect(await this.wrapper.balanceOfBatch([this.user1.address, this.user1.address], [tokenId, tokenId])).to.deep.equal([parseEther("6"), parseEther("6")]);
+    // expect(await provider.getBalance(this.user3.address)).to.equal(parseEther("0"));
+    // TODO: Heterogeneous arrays of users.
+    expect(await this.wrapper.balanceOfBatch([this.user3.address, this.user3.address], [tokenId, tokenId])).to.deep.equal([parseEther("1"), parseEther("1")]);
+
+    this.wrapper.connect(this.user1).returnToETH(parseEther("6"), this.user3.address);
+    // expect(await provider.getBalance(this.user3.address)).to.equal(parseEther("6"));
+  });
+
+  it("length mismatch", async function() {
+    const self = this;
+    {
+      async function fails() {
+        await self.wrapper.connect(self.user3).safeBatchTransferFrom(self.user1.address, self.user2.address, [tokenId, tokenId], [parseEther("1")], []);
+      }
+      await expectThrowsAsync(fails, "VM Exception while processing transaction: revert ERC1155: ids and amounts length mismatch");
+    }
+  });
+
+  it("metadata", async function() {
+    expect(await this.wrapper.name(tokenId)).to.equal("Wrapped Ether");
+    expect(await this.wrapper.symbol(tokenId)).to.equal("wETH");
+    expect(await this.wrapper.decimals(tokenId)).to.equal(18);
+    expect(await this.wrapper.uri(tokenId)).to.equal("https://example.com");
+  });
+});
