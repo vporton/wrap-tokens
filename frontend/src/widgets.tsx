a0764e68a9719478ca233984b32de843615a1af9
Victor Porton <porton.victor@gmail.com>
2022-05-28 00:25:21 +0300

    source formatting

--- frontend/src/widgets.tsx
+++ frontend/src/widgets.tsx
@@ -5,13 +5,13 @@ import {
 export function Address({...props}) {
   return (
     <span className="Address">
-    <input type="text"
-            style={{maxWidth: '23.5em', width: '100%'}}
-            maxLength={42}
-            size={50}
-            value={props.value ? props.value : ""}
-            onChange={props.onChange}
-            className={isAddressValid(props.value) ? '' : 'error'}/>
+      <input type="text"
+             style={{maxWidth: '23.5em', width: '100%'}}
+             maxLength={42}
+             size={50}
+             value={props.value ? props.value : ""}
+             onChange={props.onChange}
+             className={isAddressValid(props.value) ? '' : 'error'}/>
     </span>
 )
 }
@@ -20,12 +20,12 @@ export function Address({...props}) {
 export function Uint256({...props}) {
   return (
     <span className="Uint256">
-    <input type="text"
-            style={{maxWidth: '27em', width: '100%'}}
-            maxLength={78}
-            value={props.value}
-            onChange={props.onChange}
-            className={isUint256Valid(props.value) ? '' : 'error'}/>
+      <input type="text"
+             style={{maxWidth: '27em', width: '100%'}}
+             maxLength={78}
+             value={props.value}
+             onChange={props.onChange}
+             className={isUint256Valid(props.value) ? '' : 'error'}/>
     </span>
   )
 }
